7c26d8ade9b1c0f374b075a3eea635633b7cd49a
ca <colbyackerman@gmail.com>
2024-09-05 03:04:33 -0500

    refactor compose form into formEx

--- src/client/store/forms.js
+++ src/client/store/forms.js
@@ -1,4 +1,8 @@
-import { postFeedbackMessage, postRemark } from '../network/rhymeRemarksApi.js';
+import {
+  postFeedbackMessage,
+  postPoemComposition,
+  postRemark,
+} from '../network/rhymeRemarksApi.js';
 import remarks from './remarks.js';
 
 export default {
@@ -59,6 +63,43 @@ export default {
       pending: 'Sending ⏳',
       success: 'Sent ✅',
       Failure: 'Not Sent ⛔',
+    },
+  },
+  // ---
+  compose: {
+    fields: [
+      {
+        name: 'title',
+        label: 'Title',
+        inputType: 'text',
+        attributes: { required: true, placeholder: 'Invitation' },
+      },
+      {
+        name: 'composition',
+        label: 'Composition',
+        inputType: 'textarea',
+        attributes: {
+          required: true,
+          placeholder:
+            "If you are a dreamer, come in,\nIf you are a dreamer, a wisher, a liar\nA hope-er, a pray-er, a magic bean buyer...\nIf you're a pretender, come sit by my fire\nFor we have some flax-golden tales to spin.\nCome in!\nCome in!",
+        },
+      },
+      {
+        name: 'author',
+        label: 'Author',
+        inputType: 'text',
+        attributes: { required: true, placeholder: 'Shel Silverstein' },
+      },
+    ],
+    additionalData: {},
+    dataDefaults: {},
+    postRequestCallback: postPoemComposition,
+    refreshEventId: null,
+    submitButton: {
+      base: 'Publish',
+      pending: 'Publishing ⏳',
+      success: 'Published ✅',
+      Failure: 'Failed ⛔',
     },
   },
 };
